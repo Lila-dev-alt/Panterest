46e4950564dd0d851599ec4d5a963c1bafcbbb3a
Elisa BOURG <zaza3355@hotmail.fr>
2021-04-23 10:51:24 +0200

    method getter for fullname

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -190,4 +190,9 @@ class User implements UserInterface
 
         return $this;
     }
+    public function getFullName(): ?string
+    {
+        return $this->getFirstName() . ' ' . $this->getLastName();
+    }
+
 }
